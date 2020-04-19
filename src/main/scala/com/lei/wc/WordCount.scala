ef9c3d1d4c521a26af1425ae51f4104b3a2230b2
leizuquan1990 <843291011@qq.com>
2020-04-19 11:23:16 +0800

    # 首次上传flink应用示例

---
+++ src/main/scala/com/lei/wc/WordCount.scala
@@ -0,0 +1,37 @@
+package com.lei.wc
+
+import org.apache.flink.api.scala._
+//import org.apache.flink.api.scala.{AggregateDataSet, DataSet, ExecutionEnvironment}
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 11:11 上午 2020/4/19
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+// 批处理代码
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    // 创建一个批处理的执行环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+
+    // 从文件中读取数据
+    val inputPath = "input_dir/hello.txt"
+    val inputDataSet: DataSet[String] = env.readTextFile(inputPath)
+
+    // 分词之后做cout
+    val wordCountDataSet: AggregateDataSet[(String, Int)] = inputDataSet.flatMap(_.split(" "))
+      .map((_, 1))
+      .groupBy(0)
+      .sum(1)
+
+    // 打印输出
+    wordCountDataSet.print()
+
+      
+  }
+
+}
