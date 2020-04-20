1aeb19b969430103d1488313ece546deedc0149a
leizuquan1990 <843291011@qq.com>
2020-04-20 09:08:49 +0800

    # 新增flink数据源source之自定义数据源

--- src/main/scala/com/lei/apitest/SourceTest.scala
+++ src/main/scala/com/lei/apitest/SourceTest.scala
@@ -1,7 +1,11 @@
 package com.lei.apitest
 
+import com.lei.util.MyKafkaUtil
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+
+import scala.util.Random
 /**
  * @Author: Lei
  * @E-mail: 843291011@qq.com
@@ -11,7 +15,7 @@ import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironm
  * @Description:
  */
 
-// 将数据包装成样例类
+// 生产环境中：将数据包装成样例类
 // 温度传感器读数样例类（传感器id, 时间戳, 温度）
 case class SensorReading(id: String, timestamp: Long, temperature: Double)
 
@@ -30,12 +34,63 @@ object SourceTest {
 
     // 2、从文件中读取数据
     val stream2: DataStream[String] = env.readTextFile("input_dir/sensor.txt")
-    stream2.print("stream1").setParallelism(1)
+    //stream2.print("stream1").setParallelism(1)
 
     // 3、从Element中读取数据
-    env.fromElements(1, 2.0, "String").print()
+    //env.fromElements(1, 2.0, "String").print()
+
+    // 4、从Kafka中读取数据
+    //val stream3: DataStream[String] = env.addSource(MyKafkaUtil.getConsumer("GMALL_STARTUP"))
+    //stream3.print("stream3").setParallelism(1)
+
+    // 5、自定义source
+    val stream4: DataStream[SensorReading] = env.addSource(new SensorSource())
+    stream4.print("stream4").setParallelism(1)
 
     env.execute("source test")
 
   }
+}
+
+
+class SensorSource()extends SourceFunction[SensorReading]{
+
+  // 定义一个flag，表示数据源是否正常运行
+  var running: Boolean = true
+
+  // 取消数据源的生成
+  override def cancel(): Unit = {
+    running = false
+  }
+
+  // 正常生成数据
+  override def run(sourceContext: SourceFunction.SourceContext[SensorReading]): Unit = {
+    // 初始化一个随机数发生器
+    val random = new Random()
+
+    // 隔一段时间，传感器温度发生变化
+    // 初始化定义一组传感器温度数据
+    var curTemp = 1.to(10).map(
+      // 下一个高斯分布值，正态分布
+      i => ("sensor_" + i, 60 + random.nextGaussian() * 20)
+    )
+
+    // 用无限循环，产生数据流
+    while (running){
+      // 在前一次温度的基础上更新温度值
+      curTemp = curTemp.map(
+        t => (t._1, t._2 + random.nextGaussian())
+      )
+
+      // 获取当前时间戳
+      val curTime: Long = System.currentTimeMillis()
+      curTemp.foreach(
+        t => sourceContext.collect(SensorReading(t._1, curTime, t._2))
+      )
+
+      Thread.sleep(1000)
+    }
+  }
+
+
 }
---
+++ src/main/scala/com/lei/util/MyKafkaUtil.scala
@@ -0,0 +1,35 @@
+package com.lei.util
+
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 7:53 上午 2020/4/20
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+// flink通过有状态支持，将kafka消费的offset自动进行状态保存，自动维护偏移量
+object MyKafkaUtil {
+
+  val prop = new Properties()
+
+  val zk_servers = "node-01:9092,node-02:9092,node-03:9092"
+  prop.setProperty("bootstrap.servers", zk_servers)
+  prop.setProperty("group.id", "gmall")
+  prop.setProperty("key,deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  prop.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  prop.setProperty("auto.offset.reset", "latest")
+
+  def getConsumer(topic:String ):FlinkKafkaConsumer011[String]= {
+    val myKafkaConsumer:FlinkKafkaConsumer011[String] = new FlinkKafkaConsumer011[String](topic, new SimpleStringSchema(), prop)
+    myKafkaConsumer
+  }
+
+
+}
