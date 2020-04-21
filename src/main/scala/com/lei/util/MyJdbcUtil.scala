3dfb6406a4e2f4f6510857cf3bcc8b7f9839132a
leizuquan <843291011@qq.com>
2020-04-21 11:38:05 +0800

    # 调试成功Flink 使用druid插件将分析结果写入MySQL中

--- src/main/scala/com/lei/util/MyJdbcUtil.scala
+++ src/main/scala/com/lei/util/MyJdbcUtil.scala
@@ -1,5 +1,6 @@
 package com.lei.util
 
+import java.io.InputStream
 import java.sql.{Connection, PreparedStatement}
 import java.util.Properties
 
@@ -32,15 +33,24 @@ class MyJdbcUtil(sql:String ) extends  RichSinkFunction[Array[Any]] {
 
   //创建连接
   override def open(parameters: Configuration): Unit = {
-    val properties = new Properties()
-    properties.put("driverClassName",driver)
-    properties.put("url",url)
-    properties.put("username",username)
-    properties.put("password",password)
-    properties.put("maxActive",maxActive)
+//    val properties = new Properties()
+//    properties.put("driverClassName",driver)
+//    properties.put("url",url)
+//    properties.put("username",username)
+//    properties.put("password",password)
+//    properties.put("maxActive",maxActive)
+//
+//    val dataSource: DataSource = DruidDataSourceFactory.createDataSource(properties)
+//    connection = dataSource.getConnection()
 
-    val dataSource: DataSource = DruidDataSourceFactory.createDataSource(properties)
+//    val pro: Properties = new Properties()
+//    val ins: InputStream = MyJdbcUtil.class.getClassLoader().getResourceAsStream("druid.properties");
+//    pro.load(ConfigurationManager.getProp())
+    //获取连接池对象
+    val dataSource: DataSource  = DruidDataSourceFactory.createDataSource(ConfigurationManager.getProp())
     connection = dataSource.getConnection()
+    // 一定要注意druid.properties配置文件中的参数名一定要和上表中的名称相一致，如连接数据库的用户名为username，否则会报错。
+
   }
 
   //反复调用
