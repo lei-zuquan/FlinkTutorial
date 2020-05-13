fbba45a4127314f314f39c29691278f71691c694
leizuquan1990 <843291011@qq.com>
2020-05-13 23:05:08 +0800

    # 新增JAVA JAVA lambda表达式版本WordCount

---
+++ src/main/java/com/lei/wc/J01_WordCount.java
@@ -0,0 +1,45 @@
+package com.lei.wc;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 10:36 下午 2020/5/13
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+public class J01_WordCount {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        // 从文件中读取数据
+        //String inputPath = "input_dir/hello.txt";
+        //DataSource<String> inputDataSet = env.readTextFile(inputPath);
+        // 创建DataSet，这里我们的输入是一行一行的文本
+        DataSet<String> text = env.fromElements(
+                "Flink Spark Storm",
+                "Flink Flink Flink",
+                "Spark Spark Spark",
+                "Storm Storm Storm"
+        );
+
+        AggregateOperator<Tuple2<String, Integer>> counts = text.flatMap(
+            (String line, Collector<Tuple2<String, Integer>> collector) -> {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    collector.collect(new Tuple2<>(word, 1));
+                }
+            }
+        ).returns(Types.TUPLE(Types.STRING, Types.INT)).groupBy(0).sum(1);
+
+        counts.print();
+    }
+}
