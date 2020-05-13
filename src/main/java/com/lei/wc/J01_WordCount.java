6c21e498f7ed794bdb795d5065a8584f8eb3019d
leizuquan1990 <843291011@qq.com>
2020-05-13 23:11:55 +0800

    # 新增JAVA JAVA lambda表达式版本WordCount(本地文件读取)

--- src/main/java/com/lei/wc/J01_WordCount.java
+++ src/main/java/com/lei/wc/J01_WordCount.java
@@ -5,7 +5,9 @@ import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.util.Collector;
 
 /**
@@ -19,19 +21,21 @@ import org.apache.flink.util.Collector;
 
 public class J01_WordCount {
     public static void main(String[] args) throws Exception {
+        final ParameterTool params = ParameterTool.fromArgs(args);
+
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         // 从文件中读取数据
-        //String inputPath = "input_dir/hello.txt";
-        //DataSource<String> inputDataSet = env.readTextFile(inputPath);
+        String inputPath = "input_dir/hello.txt";
+        DataSet<String> inputDataSet = env.readTextFile(inputPath);
         // 创建DataSet，这里我们的输入是一行一行的文本
-        DataSet<String> text = env.fromElements(
+        /*DataSet<String> inputDataSet = env.fromElements(
                 "Flink Spark Storm",
                 "Flink Flink Flink",
                 "Spark Spark Spark",
                 "Storm Storm Storm"
-        );
+        );*/
 
-        AggregateOperator<Tuple2<String, Integer>> counts = text.flatMap(
+        AggregateOperator<Tuple2<String, Integer>> counts = inputDataSet.flatMap(
             (String line, Collector<Tuple2<String, Integer>> collector) -> {
                 String[] words = line.split(" ");
                 for (String word : words) {
