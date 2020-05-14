4588e8d270c10f21b2c678ab7076b83d2d21d8b4
leizuquan <843291011@qq.com>
2020-05-14 09:00:56 +0800

    # 完善项目，支持java8 lambda表达式

--- src/main/java/com/lei/wc/J01_WordCount.java
+++ src/main/java/com/lei/wc/J01_WordCount.java
@@ -1,11 +1,9 @@
 package com.lei.wc;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.AggregateOperator;
-import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.util.Collector;
