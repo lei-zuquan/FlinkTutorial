b0def5d6bfdd51088617db92cdf4a60b31e94ded
leizuquan1990 <843291011@qq.com>
2020-06-07 12:00:28 +0800

    # 新增 C01_PrintSink

---
+++ src/main/java/com/lei/apitest/c03_sink/C01_PrintSink.java
@@ -0,0 +1,28 @@
+package com.lei.apitest.c03_sink;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 11:51 上午 2020/6/7
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+public class C01_PrintSink {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> lines = env.socketTextStream("localhost", 7777);
+
+        // print时显示的数字哪里来的，代表什么含义：PrintSinkOutputWriter.java
+        // completedPrefix += (subtaskIndex + 1);
+        // completedPrefix += "> ";
+
+        // print是测试时使用，生产环境与其他存储介质结合使用
+        lines.print("res").setParallelism(2);
+
+        env.execute("C01_PrintSink");
+    }
+}
