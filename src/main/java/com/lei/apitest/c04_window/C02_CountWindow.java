81fe750c94d40799c78dbb9861ccd8762efee8ad
leizuquan1990 <843291011@qq.com>
2020-06-07 15:37:34 +0800

    # 新增 C02_CountWindow

---
+++ src/main/java/com/lei/apitest/c04_window/C02_CountWindow.java
@@ -0,0 +1,52 @@
+package com.lei.apitest.c04_window;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.*;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 3:18 下午 2020/6/7
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+/*
+ 分组后再调用CountWindow，每一个组达到一定的条数才会触发任务执行
+ */
+public class C02_CountWindow {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // spark,3
+        // hadoop,2
+        DataStreamSource<String> lines = env.socketTextStream("localhost", 7777);
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> wordAndCount = lines.map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String value) throws Exception {
+                String[] fileds = value.split(",");
+                String word = fileds[0];
+                Integer count = Integer.parseInt(fileds[1]);
+                return Tuple2.of(word, count);
+            }
+        });
+
+        // 先分组，再划分窗口
+        KeyedStream<Tuple2<String, Integer>, Tuple> keyed = wordAndCount.keyBy(0);
+
+        // 划分窗口
+        WindowedStream<Tuple2<String, Integer>, Tuple, GlobalWindow> window = keyed.countWindow(5);
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> summed = window.sum(1);
+
+        summed.print();
+
+        env.execute("C02_CountWindow");
+    }
+}
