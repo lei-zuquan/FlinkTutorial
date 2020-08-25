7e91dfedacc89673a2fb3ad30912fc167e59dedb
leizuquan1990 <843291011@qq.com>
2020-08-25 09:20:34 +0800

    # 修正Flink 注释

--- src/main/java/com/lei/apitest/c05_project/C06_StateBackendDemo.java
+++ src/main/java/com/lei/apitest/c05_project/C06_StateBackendDemo.java
@@ -27,20 +27,20 @@ public class C06_StateBackendDemo {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // 只有开启了checkpointing 才会有重启策略
+        // 只有开启了checkpointing，重启策略才会生效；默认不开启重启策略
         env.enableCheckpointing(5000); // 开启，检查点周期，单位毫秒；默认是-1，不开启
 
         // 默认的重启策略是固定延迟无限重启
         //env.getConfig().setRestartStrategy(RestartStrategies.fallBackRestart());
-        // 设置固定延迟固定次数重启，默认是无限重启
+        // 设置固定延迟固定次数重启；默认是无限重启
         env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 1000));
 
-        // 设置状态数据存储的后端，本地文件系统
+        // 设置状态数据存储的后端，本地文件系统；默认：状态保存在 TaskManager 的内存中，检查点保存在 JobManager 的内存中
         env.setStateBackend(new FsStateBackend("file:\\\\lei_test_project\\idea_workspace\\FlinkTutorial\\check_point_dir"));
         // 生产环境将StateBackend保存到分布式文件系统
         //env.setStateBackend(new FsStateBackend("hdfs://node-01:8020/user/root/sqoop/flink_state_backend"));
 
-        // 程序异常退出或人为cancel掉，不删除checkpoint的数据
+        // 程序异常退出或人为cancel掉，不删除checkpoint的数据；默认是会删除Checkpoint数据
         env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
         DataStreamSource<String> lines = env.socketTextStream("node-01", 7777);
