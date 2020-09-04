20920561b8d099539f2c53eb013f11fed8ea11d8
leizuquan1990 <843291011@qq.com>
2020-09-04 14:03:49 +0800

    # 配置 Flink 状态后端采用MemoryStateBackend时，使用异步快照来防止数据流阻塞

--- src/main/java/com/lei/apitest/c05_project/C05_RestartStrategiesDemo.java
+++ src/main/java/com/lei/apitest/c05_project/C05_RestartStrategiesDemo.java
@@ -3,6 +3,8 @@ package com.lei.apitest.c05_project;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.StateBackend;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -22,32 +24,30 @@ public class C05_RestartStrategiesDemo {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // 只有开启了checkpoint 才会有重启策略，
-        // 默认保存到内存中，即检查点保存在JobManager内存中，状态保存在TaskManager内存中
+        // 只有开启了checkpoint 才会有重启策略  // 默认保存到内存中，即检查点保存在JobManager内存中，状态保存在TaskManager内存中
         env.enableCheckpointing(5000); // 开启，检查点周期，单位毫秒；默认是-1，不开启
         // 高级选项：
         // 设置模式为精确一次 (这是默认值)
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
         // 确认 checkpoints 之间的时间会进行 500 ms
         env.getCheckpointConfig().setMinPauseBetweenCheckpoints(500);
-
         // Checkpoint 必须在一分钟内完成，否则就会被抛弃
         env.getCheckpointConfig().setCheckpointTimeout(60000);
-
         // 同一时间只允许一个 checkpoint 进行
         env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
-
         // 开启在 job 中止后仍然保留的 externalized checkpoints
         env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
-
         // 允许在有更近 savepoint 时回退到 checkpoint
         env.getCheckpointConfig().setPreferCheckpointForRecovery(true);
 
 
         // 默认的重启策略是固定延迟无限重启
-        //env.getConfig().setRestartStrategy(RestartStrategies.fallBackRestart());
-        // 设置固定延迟固定次数重启
+        // env.getConfig().setRestartStrategy(RestartStrategies.fallBackRestart());
+        // 设置固定延迟固定次数重启，重启3次，每次重启间隔2秒
         env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 2000));
+        // 强烈建议使用异步快照来防止数据流阻塞
+        StateBackend memoryStateBackend = new MemoryStateBackend(MemoryStateBackend.DEFAULT_MAX_STATE_SIZE, true);
+        env.setStateBackend(memoryStateBackend);
 
         DataStreamSource<String> lines = env.socketTextStream("node-01", 7777);
         SingleOutputStreamOperator<Tuple2<String, Integer>> wordAndOne = lines.map(new MapFunction<String, Tuple2<String, Integer>>() {
